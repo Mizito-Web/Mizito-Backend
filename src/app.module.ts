5b3f5b9ec359db4f9530b7716dcecda45e786e15
Mohammad matin <matin.ghiasvand1381@gmail.com>
2025-01-10 10:52:48 +0330

    Add modules for base routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { configDotenv } from 'dotenv';
+import { ProjectModule } from './modules/project/project.module';
+import { TasksModule } from './modules/tasks/tasks.module';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ChatModule } from './modules/chat/chat.module';
+configDotenv();
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URL), ProjectModule, TasksModule, UsersModule, AuthModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
