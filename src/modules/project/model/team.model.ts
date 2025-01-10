5745306b8ba265a586ae84de7f0ab19f9b897f0d
Mohammad matin <matin.ghiasvand1381@gmail.com>
2025-01-10 14:35:49 +0330

    Add team model

---
+++ src/modules/project/model/team.model.ts
@@ -0,0 +1,31 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+import { Date, Document, now } from 'mongoose';
+
+@Schema()
+export class Team {
+  _id: string;
+
+  @ApiProperty({
+    example: 'K3rn3lpanic team',
+    description: 'Name of the team',
+  })
+  @Prop({ required: true })
+  name: string;
+
+  @ApiProperty({
+    example: 'https://example.com/image.png',
+    description: 'The image url of the team',
+  })
+  @Prop({ required: false })
+  imageUrl: string;
+
+  @Prop({ default: now })
+  createdAt: Date;
+
+  @Prop({ default: now })
+  updatedAt: Date;
+}
+
+export type TaskDocument = Team & Document;
+export const UserSchema = SchemaFactory.createForClass(Team);
